6689fe056ebc12f9fe9499badb140a628992c3c1
fares-fraoucene <fares.fraoucene@laplateforme.io>
2023-11-20 12:17:00 +0100

    Ajout jobs 9, 10

--- jour01/job09/inventaire.py
+++ jour01/job09/inventaire.py
@@ -1,13 +1,18 @@
 import string
 
-nom=f""
-prix =f""
-unitaire=f""
-quantité_en_stock=f""
-quantité_en_stock+100
-print(quantité_en_stock)
+nom="i9-14900k"
+prix_unitaire = 676.66
+quantité_en_stock= 20
+inflation10 = prix_unitaire * 10 / 100
+prix_post_infla = prix_unitaire + inflation10
 
-nb_produit_souhaité = input(0)
+print(f"Nom :{nom}, Prix : {prix_post_infla}€, Quantité{quantité_en_stock}.")
+nb_produit_souhaité = input()
 
+if  int(nb_produit_souhaité) <=  quantité_en_stock:
+    quantité_en_stock = quantité_en_stock - int(nb_produit_souhaité)
+    # (quantité_en_stock) -= int(nb_produit_souhaité) 
+else:
+    print("Désolé le produit et hors stock")
 
-
+print(f"Nom :{nom}, Prix : {prix_unitaire}€, Quantité{quantité_en_stock}")
